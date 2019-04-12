22b2d89096cfe989df7bef0bcf6ab405b4cf6621
Thiago Diniz <thiagopaesldiniz@gmail.com>
2019-04-12 17:05:32 -0300

    Update D20.py

--- Testes/D20.py
+++ Testes/D20.py
@@ -1,13 +1,40 @@
 import random
-dice = [1, 2, 3, 4, 5, 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-resposta = 1;
-while(resposta == 1):
-	input("Para girar o dado, tecle ENTER:\n");
-	num_sorteado = random.choice(dice);
-	print (num_sorteado);
-	continua = input("Deseja executar o programa novamente?(Digite 'S' para sim e 'N' para não)\n");
-	continua = continua.lower();
-	if(continua == 's'):
-		resposta = 1;
+dice = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+answer = 1;
+while(answer == 1):
+	error_loop = 0;
+	input("To roll the dice, click ENTER:\n");
+	sort_number = random.choice(dice);
+	print (sort_number);
+	re_roll = input("Do you want to roll the dice again?(Click 'Y' to YES and 'N' to NO)\n");
+	re_roll = re_roll.lower();
+	if(re_roll == 'y'):
+		answer = 1;
+		error_loop = 0;
+	elif re_roll == 'n':
+		answer = 0;
+		error_loop = 0;
 	else:
-		resposta = 0;
+		error_loop = 1;
+	
+	while(error_loop == 1 and answer == 1):
+		if(re_roll == 'y'):
+			answer = 1;
+			error_loop = 0;
+		elif re_roll == 'n':
+			answer = 0;
+			error_loop = 0;
+		else:
+			print ("Invalid character!");
+			re_roll = input("Do you want to roll the dice again?(Click 'Y' to YES and 'N' to NO)\n");
+			re_roll = re_roll.lower();
+			if re_roll == 'y':
+				error_loop = 0;
+				answer = 1;
+			elif re_roll == 'n':
+				error_loop = 0;
+				answer = 0;
+			else:
+				error_loop = 1;
+
+print("\nThank you for use this program!\n");
